b5825f813cf833d0a36bd03e015b7fc55d5d9f0d
Emrahgs <bagirovemrah97@gmail.com>
2021-09-20 03:01:52 +0400

    readme2

--- DevelopsToday/settings.py
+++ DevelopsToday/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "DevelopsToday.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
